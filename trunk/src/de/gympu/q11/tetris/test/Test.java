d7efed6a5b7ee6b6a1d3e4fd06d1f74502149539
christian.spitschka@gmail.com <christian.spitschka@gmail.com@d5dd1984-93c7-4c7e-70e8-fd34161c26bf>
2012-06-22 07:07:14 +0000

    hash Code

--- trunk/src/de/gympu/q11/tetris/test/Test.java
+++ trunk/src/de/gympu/q11/tetris/test/Test.java
@@ -1,5 +1,15 @@
 package de.gympu.q11.tetris.test;
 
 public class Test {
+	
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
 
 }
