5ab9ff7283ebb1ccfd95d2ee73955256f1498e6d
christian.spitschka@gmail.com <christian.spitschka@gmail.com@d5dd1984-93c7-4c7e-70e8-fd34161c26bf>
2012-06-22 07:08:48 +0000

    tzu

--- src/de/gympu/q11/tetris/test/Test.java
+++ src/de/gympu/q11/tetris/test/Test.java
@@ -1,15 +1,5 @@
 package de.gympu.q11.tetris.test;
 
 public class Test {
-	
-	public Test() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	@Override
-	public int hashCode() {
-		// TODO Auto-generated method stub
-		return super.hashCode();
-	}
 
 }
