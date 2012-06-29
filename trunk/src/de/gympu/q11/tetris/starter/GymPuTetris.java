a85bcdc4d1dafb0c63d9f5028831fead1b9629ee
christian.spitschka@gmail.com <christian.spitschka@gmail.com@d5dd1984-93c7-4c7e-70e8-fd34161c26bf>
2012-06-29 06:46:49 +0000

    ddd

---
+++ trunk/src/de/gympu/q11/tetris/starter/GymPuTetris.java
@@ -0,0 +1,20 @@
+package de.gympu.q11.tetris.starter;
+
+import de.gympu.q11.tetris.view.TetrisView;
+
+public class GymPuTetris {
+	
+	private TetrisView view;
+	
+	public GymPuTetris() {
+		view = new TetrisView();
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new GymPuTetris();
+	}
+
+}
