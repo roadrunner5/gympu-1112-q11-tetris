554fe7b8d40d82893fff0d67341128fbc4544151
christian.spitschka@gmail.com <christian.spitschka@gmail.com@d5dd1984-93c7-4c7e-70e8-fd34161c26bf>
2012-07-05 09:55:04 +0000

    Paint

--- src/Testpackage/Testklasse.java
+++ src/Testpackage/Testklasse.java
@@ -1,5 +1,42 @@
 package Testpackage;
 
-public class Testklasse {
+import java.io.File;
+
+import javax.swing.JFrame;
+
+public class Testklasse extends JFrame {
+	public Testklasse() {
+		super("Testfenster");
+		
+		this.setResizable(false);
+		this.setUndecorated(true);
+		
+		Zeichenpanel z = new Zeichenpanel(new File("test.jpg"));
+		this.getContentPane().add(z);
+		this.setSize(600, 600);
+		this.setLocation(2200, 100);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		try {
+			Thread.sleep(2000);
+			z.datei = new File("test2.jpg");
+			z.repaint(50, 50, 50, 50);
+			
+			
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	}
+	
+	public static void main(String[] a) {
+		new Testklasse();
+	}
 
 }
---
+++ src/Testpackage/Zeichenpanel.java
@@ -0,0 +1,30 @@
+package Testpackage;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class Zeichenpanel extends JPanel {
+	
+	public File datei;
+	public Zeichenpanel(File f) {
+		datei = f;
+	}
+	
+	
+	public void paint(Graphics g) {
+		Image img;
+		try {
+			img = ImageIO.read(datei);
+			g.drawImage(img, 50, 50, null);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
