8fe4f9f7e3a0a8937bbc05c92f92cbbe482da006
maltejojo95@gmail.com <maltejojo95@gmail.com@d5dd1984-93c7-4c7e-70e8-fd34161c26bf>
2012-07-05 20:21:21 +0000

    Musik

--- src/de/gympu/q11/tetris/view/TetrisKeyListener.java
+++ src/de/gympu/q11/tetris/view/TetrisKeyListener.java
@@ -6,53 +6,54 @@ import java.awt.event.KeyListener;
 import de.gympu.q11.tetris.controller.TetrisController;
 
 public class TetrisKeyListener implements KeyListener {
-	private TetrisController controler;
-	
-	public TetrisKeyListener(TetrisController c) {
-		this.controler = c;
-	}
-	
-	public void keyPressed(KeyEvent arg0) {
-//		System.out.println("keyP");
-		int code = arg0.getKeyCode();
-		
-		if(code == KeyEvent.VK_LEFT) {
-			System.out.println("links");
-			this.controler.links();
-		}
-		
-		if(code == KeyEvent.VK_RIGHT) {
-			System.out.println("rechts");
-			this.controler.rechts();
-		}
-		
+        private TetrisController controler;
+        
+        public TetrisKeyListener(TetrisController c) {
+                this.controler = c;
+        }
+        
+        public void keyPressed(KeyEvent arg0) {
+//              System.out.println("keyP");
+                int code = arg0.getKeyCode();
+                
+                if(code == KeyEvent.VK_LEFT) {
+                        System.out.println("links");
+                        this.controler.links();
+                }
+                
+                if(code == KeyEvent.VK_RIGHT) {
+                        System.out.println("rechts");
+                        this.controler.rechts();
+                }
+                
 
-		if(code == KeyEvent.VK_DOWN) {
-			System.out.println("unten");
-			this.controler.unten();
-		}		
-		
-	
-			
-		if(code == KeyEvent.VK_SPACE) {
-			System.out.println("space");
-			this.controler.gegenUhrzeigerSinnDrehen();
-		}
-		
-		if(code == KeyEvent.VK_P) {
-			System.out.println("pause");
-			this.controler.pause();
-		}	
-	}
-		
-	
+                if(code == KeyEvent.VK_DOWN) {
+                        System.out.println("unten");
+                        this.controler.unten();
+                }               
+                
+        
+                        
+                if(code == KeyEvent.VK_SPACE) {
+                        System.out.println("space");
+                        this.controler.gegenUhrzeigerSinnDrehen();
+                }
+                
+                if(code == KeyEvent.VK_P) {
+                        System.out.println("pause");
+                        this.controler.pause();
+                }       
+        }
+                
+        
 
-	public void keyReleased(KeyEvent arg0) {
-//		System.out.println("keyR");
-	}
+        public void keyReleased(KeyEvent arg0) {
+//              System.out.println("keyR");
+        }
 
-	public void keyTyped(KeyEvent arg0) {
-//		System.out.println("keyT");
-	}
+        public void keyTyped(KeyEvent arg0) {
+//              System.out.println("keyT");
+        }
 
 }
+
